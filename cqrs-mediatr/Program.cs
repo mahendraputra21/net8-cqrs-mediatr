cb11bf30ec886cb31e4ede0c71dd95c0325994ce
dewa-mahendra-laptop <mahendraputra21@gmail.com>
2024-05-22 22:57:49 +0800

    add routing for each endpoints

--- cqrs-mediatr/Program.cs
+++ cqrs-mediatr/Program.cs
@@ -5,6 +5,7 @@ using cqrs_mediatr.Features.Products.Notifications;
 using cqrs_mediatr.Features.Products.Queries.Get;
 using cqrs_mediatr.Features.Products.Queries.List;
 using cqrs_mediatr.Persistence;
+using cqrs_mediatr.Routing;
 using MediatR;
 using System.Reflection;
 
@@ -25,41 +26,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/products/{id:guid}", async (Guid id, ISender mediatr) =>
-{
-    var product = await mediatr.Send(new GetProductQuery(id));
-    if (product == null) return Results.NotFound();
-    return Results.Ok(product);
-});
-
-app.MapGet("/products", async (ISender mediatr) =>
-{
-    var products = await mediatr.Send(new ListProductsQuery());
-    return Results.Ok(products);
-});
-
-app.MapPost("/products", async (CreateProductCommand command, IMediator mediatr) =>
-{
-    var productId = await mediatr.Send(command);
-    if(Guid.Empty == productId) return Results.BadRequest();
-    await mediatr.Publish(new ProductCreatedNotification(productId));
-    return Results.Created($"/products/{productId}", new { id = productId});
-});
-
-app.MapPut("/products", async (UpdateProductCommand command, ISender mediatr) =>
-{
-    var productId = await mediatr.Send(command);
-    if (Guid.Empty == productId) return Results.BadRequest();
-    return Results.Ok($"Update Product on Id: {productId} successfully");
-});
-
-app.MapDelete("/products/{id:guid}", async (Guid id, ISender mediatr) =>
-{
-    await mediatr.Send(new DeleteProductCommand(id));
-    return Results.NoContent();
-});
-
 app.UseHttpsRedirection();
+app.UseRouting();
+// Register Routing endpoints
+Routing.MapRoutes(app);
+
 app.Run();
 
 
