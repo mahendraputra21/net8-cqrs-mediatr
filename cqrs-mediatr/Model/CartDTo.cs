de8400097596b6b748333d04b8903f19497b9420
dewa-mahendra-laptop <mahendraputra21@gmail.com>
2024-06-05 22:20:11 +0800

    refactor routing parameters

--- cqrs-mediatr/Model/CartDTo.cs
+++ cqrs-mediatr/Model/CartDTo.cs
@@ -4,5 +4,6 @@ namespace cqrs_mediatr.Model
 {
     public record CartDto(Guid? Id, List<CartItemDto> Items, decimal TotalPrice);
     public record CartItemDto(Guid? ProductId, string ProductName, string ProductDescription, int Quantity);
+    public record CreateCartRequest(Guid CartId, Guid ProductId, int Quatity);
 
 }
