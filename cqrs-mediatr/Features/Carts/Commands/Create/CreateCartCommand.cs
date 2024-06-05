8e8cc2429b9718fc82794588de271935f7788fcf
dewa-mahendra-laptop <mahendraputra21@gmail.com>
2024-06-05 22:49:45 +0800

    fix issue Cannot consume scoped service 'cqrs_mediatr.Persistence.IUnitOfWork' from singleton

--- cqrs-mediatr/Features/Carts/Commands/Create/CreateCartCommand.cs
+++ cqrs-mediatr/Features/Carts/Commands/Create/CreateCartCommand.cs
@@ -4,23 +4,5 @@ using Mediator;
 
 namespace cqrs_mediatr.Features.Carts.Commands.Create
 {
-    public class CreateCartCommand : IRequest<CartDto>
-    {
-        private readonly IGuidGenerator _guidGenerator;
-        private readonly IInt32Generator _int32Generator;
-
-        public Guid CartId { get; private set; }
-        public Guid ProductId { get; private set; } 
-        public int Quantity { get; private set; }
-
-        public CreateCartCommand(Guid CartId, Guid ProductId, int Quantity, IGuidGenerator typeDataGenerator, IInt32Generator int32Generator)
-        {
-            _guidGenerator = typeDataGenerator;
-            _int32Generator = int32Generator;
-            CartId = _guidGenerator.GenerateGuid();
-            ProductId = _guidGenerator.GenerateGuid();
-            Quantity = _int32Generator.GenerateInt32();
-            
-        }
-    }
+    public record CreateCartCommand(Guid CartId, Guid ProductId, int Quantity) : IRequest<CartDto>;
 }
--- cqrs-mediatr/Program.cs
+++ cqrs-mediatr/Program.cs
@@ -1,13 +1,7 @@
 using cqrs_mediatr.Behaviors;
 using cqrs_mediatr.Exceptions;
-using cqrs_mediatr.Features.Carts.Commands.Create;
-using cqrs_mediatr.Features.Products.Commands.Create;
-using cqrs_mediatr.Features.Products.Commands.Delete;
-using cqrs_mediatr.Features.Products.Commands.Update;
-using cqrs_mediatr.Features.Products.Queries.Get;
 using cqrs_mediatr.Persistence;
 using cqrs_mediatr.Repositories;
-using cqrs_mediatr.Repositories.Configuration;
 using cqrs_mediatr.Routing;
 using FluentValidation;
 using Mediator;
@@ -28,9 +22,6 @@ builder.Services.AddMediator(options =>
     options.ServiceLifetime = ServiceLifetime.Scoped;
 });
 
-builder.Services.AddSingleton<IGuidGenerator, DefaultGuidGenerator>();
-builder.Services.AddSingleton<IInt32Generator, Int32Generator>();
-
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
@@ -39,48 +30,6 @@ builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped(typeof(RequestResponseLoggingBehavior<,>));
 builder.Services.AddScoped(typeof(ValidationBehavior<,>));
 builder.Services.AddScoped(typeof(DBContextTransactionPipelineBehavior<,>));
-
-
-builder.Services.AddScoped<CreateCartCommand>();
-
-builder.Services.AddScoped<CreateProductCommand>(provider =>
-{
-    var name = "example";
-    var description = "example";
-    var price = 0;
-    return new CreateProductCommand(name, description, price);
-});
-builder.Services.AddScoped<UpdateProductCommand>(provider =>
-{
-    var id = Guid.NewGuid(); // Replace with actual logic to get Guid
-    var name = "example";
-    var description = "example";
-    var price = 0;
-    return new UpdateProductCommand(id, name, description, price);
-});
-
-builder.Services.AddScoped<DeleteProductCommand>(provider =>
-{
-    var guid = Guid.NewGuid(); // Replace with actual logic to get Guid
-    return new DeleteProductCommand(guid);
-});
-
-builder.Services.AddScoped<GetProductQuery>(provider =>
-{
-    var guid = Guid.NewGuid(); // Replace with actual logic to get Guid
-    return new GetProductQuery(guid);
-});
-
-
-builder.Services.AddScoped<GetProductQueryHandler, GetProductQueryHandler>();
-builder.Services.AddScoped<CreateCartCommandHandler, CreateCartCommandHandler>();
-builder.Services.AddScoped<CreateProductCommandHandler, CreateProductCommandHandler>();
-builder.Services.AddScoped<UpdateProductCommandHandler, UpdateProductCommandHandler>();
-builder.Services.AddScoped<DeleteProductCommandHandler, DeleteProductCommandHandler>();
-
-builder.Services.AddScoped<CreateProductCommandValidator, CreateProductCommandValidator>();
-builder.Services.AddScoped<UpdateProductCommandValidator, UpdateProductCommandValidator>();
-
 
 // Registering Automapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
