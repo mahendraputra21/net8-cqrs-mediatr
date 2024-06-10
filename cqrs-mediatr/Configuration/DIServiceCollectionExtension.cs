4a351708d59aacf17468f49204f6ddf411fdc5ad
Dewa Mahendra <d.putra@lakeba.com>
2024-06-10 13:33:59 +0800

    add group of dependency Injection

---
+++ cqrs-mediatr/Configuration/DIServiceCollectionExtension.cs
@@ -0,0 +1,53 @@
+﻿using cqrs_mediatr.Behaviors;
+using cqrs_mediatr.Exceptions;
+using cqrs_mediatr.Persistence;
+using cqrs_mediatr.Repositories;
+using FluentValidation;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace cqrs_mediatr.Configuration
+{
+    public static class DIServiceCollectionExtension
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            // Registering DB Context
+            services.AddDbContext<AppDbContext>();
+
+            // Registering AutoMapper
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            // Registering Pipeline Behaviours
+            services.AddScoped(typeof(RequestResponseLoggingBehavior<,>));
+            services.AddScoped(typeof(ValidationBehavior<,>));
+            services.AddScoped(typeof(DBContextTransactionPipelineBehavior<,>));
+
+            // Registering Fluent Validator
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Registering Global Exception Handler
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+
+            // Registering Problem Details
+            services.AddProblemDetails(options =>
+            {
+                options.CustomizeProblemDetails = (context) =>
+                {
+                    if (!context.ProblemDetails.Extensions.ContainsKey("traceId"))
+                    {
+                        string? traceId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier;
+                        context.ProblemDetails.Extensions.Add(new KeyValuePair<string, object?>("traceId", traceId));
+                    }
+                };
+            });
+
+            // Registering Repositories
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICartRepository, CartRepository>();
+
+            return services;
+        }
+    }
+}
--- cqrs-mediatr/Program.cs
+++ cqrs-mediatr/Program.cs
@@ -1,20 +1,9 @@
-using cqrs_mediatr.Behaviors;
-using cqrs_mediatr.Exceptions;
-using cqrs_mediatr.Persistence;
-using cqrs_mediatr.Repositories;
 using cqrs_mediatr.Routing;
-using FluentValidation;
-using Mediator;
-using System.Diagnostics;
-using System.Reflection;
-
+using cqrs_mediatr.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Registering DB Context
-builder.Services.AddDbContext<AppDbContext>();
 
 // Registering MediatR add pipeline behaviour
 builder.Services.AddMediator(options =>
@@ -22,39 +11,8 @@ builder.Services.AddMediator(options =>
     options.ServiceLifetime = ServiceLifetime.Scoped;
 });
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-
-// Register behaviors
-builder.Services.AddScoped(typeof(RequestResponseLoggingBehavior<,>));
-builder.Services.AddScoped(typeof(ValidationBehavior<,>));
-builder.Services.AddScoped(typeof(DBContextTransactionPipelineBehavior<,>));
-
-// Registering Automapper
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-// registering Fluent Validator
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-// Registering Global Exception handler
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-// Registering Problem Details
-builder.Services.AddProblemDetails( options =>
-{
-    options.CustomizeProblemDetails = (context) => 
-    {
-        if (!context.ProblemDetails.Extensions.ContainsKey("traceId"))
-        {
-            string? traceId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier;
-            context.ProblemDetails.Extensions.Add(new KeyValuePair<string, object?>("traceId", traceId));
-        }
-    };
-});
-
 // Registeering DI
-//builder.Services.AddRepositories();
+builder.Services.AddDependencyInjection();
 
 var app = builder.Build();
 
